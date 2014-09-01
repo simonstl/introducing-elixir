d869967b4585c7eea469136ad69404417367e04d
Simon St.Laurent <simonstl@simonstl.com>
2014-08-31 20:52:42 -0400

    ch03

--- code/ch02/ex7-docs/drop.ex
+++ code/ch02/ex7-docs/drop.ex
@@ -1,13 +1,15 @@
 defmodule Drop do
+
   @moduledoc """
   Functions calculating velocities achieved by objects dropped in a vacuum.
   
-  from *Introducing Elixir*, O'Reilly Media, Inc., 2014.  
+  from *Introducing Elixir*, O'Reilly Media, Inc., 2014.
   Copyright 2014 by Simon St.Laurent and J. David Eisenberg.
-  """
+"""
+
   @vsn 0.1
 
-@doc """ 
+  @doc """ 
 Calculates the velocity of an object falling on Earth
 as if it were in a vacuum (no air resistance).  The distance is
 the height from which the object falls, specified in meters,
