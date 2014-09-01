853b29c4e731280ca32bcb70e364fdf98462ae22
Simon St.Laurent <simonstl@simonstl.com>
2014-09-01 08:47:49 -0400

    ch07 phase 2

--- code/ch07/ex-protocol/tower.ex
+++
@@ -1,9 +0,0 @@
-defmodule Tower do
-  defstruct location: "", height: 20, planemo: :earth, name: ""
-end
-
-defimpl Valid, for: Tower do
-  def valid?(%Tower{height: h, planemo: p}) do
-    h >= 0 && p != nil
-  end
-end
--- code/ch07/ex1-hof/hof.ex
+++
@@ -1,5 +0,0 @@
-defmodule Hof do
-  def tripler(value, function) do
-    3 * function.(value)
-  end
-end
