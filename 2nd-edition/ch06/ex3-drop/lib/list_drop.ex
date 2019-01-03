f71515703ec1a456b190c9d0a4b25948937ec30c
Anatolij <developer@languages-with-music.com>
2019-01-03 20:36:51 +0200

    Add source code to the empty file

--- 2nd-edition/ch06/ex3-drop/lib/list_drop.ex
+++ 2nd-edition/ch06/ex3-drop/lib/list_drop.ex
@@ -1,2 +1,13 @@
 defmodule ListDrop do
+  def falls(list) do
+    falls(list, [])
+  end
+
+  def falls([head|tail], results) do
+    falls(tail, [Drop.fall_velocity(head) | results])
+  end
+  
+  def falls([], results) do
+    Enum.reverse(results)
+  end
 end
