845cc983c27e785d6bb689be368e59039efa732b
Anatolij <developer@languages-with-music.com>
2019-05-30 12:50:12 +0200

    Format code, add idiomatic form to char_to_planemo
    Format source code, fix warnings, add idiomatic form to function `char_to_planemo`

--- 2nd-edition/ch05/ex3-ask/lib/ask.ex
+++ 2nd-edition/ch05/ex3-ask/lib/ask.ex
@@ -1,36 +1,31 @@
 defmodule Ask do
-
   def line() do
-    planemo=get_planemo()
-    distance=get_distance()
+    planemo = get_planemo()
+    distance = get_distance()
     Drop.fall_velocity({planemo, distance})
   end
 
+  defp char_to_planemo("1"), do: :earth
+  defp char_to_planemo("2"), do: :moon
+  defp char_to_planemo("3"), do: :mars
+  
   defp get_planemo() do
-    IO.puts("""
+    IO.puts(
+	"""
     Which planemo are you on?
-     1. Earth
-     2. Earth's Moon
-     3. Mars
-     """)
+    1. Earth
+    2. Earth's Moon
+    3. Mars
+    """)
 
     IO.gets("Which? > ")
     |> String.first()
     |> char_to_planemo()
   end
 
-defp get_distance() do
-   IO.gets("How far? (meters) > ")
-   |> String.strip()
-   |> String.to_integer()
-end
-
-defp char_to_planemo(char) do
-   case char do
-       "1" -> :earth
-       "2" -> :moon
-       "3" -> :mars
-   end
+  defp get_distance() do
+    IO.gets("How far? (meters) > ")
+    |> String.trim()
+    |> String.to_integer()
   end
-
 end
